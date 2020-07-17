d2bf90e4df97a5e80929da6c38b1b086864ae106
Saul Gallegos <saulladrondeguevara@gmail.com>
2020-07-16 19:57:48 -0500

    Screen limits and read key down

--- main.py
+++ main.py
@@ -4,17 +4,45 @@ pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
 
-
-pygame.display.set_caption("Space Invaders")
+# Caption and Icon
+pygame.display.set_caption("Mamberrois Espaciales")
 icon = pygame.image.load('logo.png')
 pygame.display.set_icon(icon)
 
+# Player
+playerImg = pygame.image.load('space.png')
+playerX = 370
+playerY = 480
+playerX_change = 0
 
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+
+# Game Loop
 running = True
 while running:
+    screen.fill((5, 23, 72))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((100, 100, 100))
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+    playerX += playerX_change
+
+    if playerX <= 0:
+        playerX = 0
+    if playerX >= 736:
+        playerX = 736
+
+    player(playerX, playerY)
     pygame.display.update()
